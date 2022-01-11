086a8ecec7faeb7e73dbc8c2b6a95998cb26141b
msuttt <35849489+msuttt@users.noreply.github.com>
2022-01-11 20:41:40 +0300

    Proje dosyası ekle.

---
+++ 12M/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace _12M
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtFinish = new System.Windows.Forms.TextBox();
+            this.txtProductCode = new System.Windows.Forms.TextBox();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.dtResult = new System.Windows.Forms.DataGridView();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Başlangıç:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(326, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bitiş:";
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(117, 48);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(161, 22);
+            this.txtStart.TabIndex = 4;
+            // 
+            // txtFinish
+            // 
+            this.txtFinish.Location = new System.Drawing.Point(370, 48);
+            this.txtFinish.Name = "txtFinish";
+            this.txtFinish.Size = new System.Drawing.Size(172, 22);
+            this.txtFinish.TabIndex = 5;
+            // 
+            // txtProductCode
+            // 
+            this.txtProductCode.Location = new System.Drawing.Point(671, 48);
+            this.txtProductCode.Name = "txtProductCode";
+            this.txtProductCode.Size = new System.Drawing.Size(124, 22);
+            this.txtProductCode.TabIndex = 6;
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(894, 48);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(125, 22);
+            this.txtProductName.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(594, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Mal.Kodu:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(830, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Mal.Adı:";
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(426, 111);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 23);
+            this.btnGet.TabIndex = 10;
+            this.btnGet.Text = "ARA";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // dtResult
+            // 
+            this.dtResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtResult.Location = new System.Drawing.Point(41, 176);
+            this.dtResult.Name = "dtResult";
+            this.dtResult.RowHeadersWidth = 51;
+            this.dtResult.RowTemplate.Height = 24;
+            this.dtResult.Size = new System.Drawing.Size(978, 251);
+            this.dtResult.TabIndex = 11;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(568, 111);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Temizle";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1061, 477);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.dtResult);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.txtProductCode);
+            this.Controls.Add(this.txtFinish);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtFinish;
+        private System.Windows.Forms.TextBox txtProductCode;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.DataGridView dtResult;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ DAL/CRUD.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class CRUD
+    {
+        public void Save(IDbCommand command)
+        {
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public void Delete(IDbCommand command)
+        {
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public void Update(IDbCommand command)
+        {
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+        }
+      
+
+        public DataTable List(string procedureName, SqlConnection con,string MalKodu,string Start,string Finish)
+        {
+
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlCommand command = new SqlCommand(procedureName, con);
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("Malkodu", MalKodu);
+                command.Parameters.AddWithValue("Baslangic", Start);
+                command.Parameters.AddWithValue("Bitis", Finish);
+
+                SqlDataReader dr = command.ExecuteReader();
+
+              
+                if (dr.Read())
+                {
+                    foreach(var item in dr)
+                    {
+                        dt.Rows.Add(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            return dt;
+        }
+
+    }
+}
